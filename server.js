6ba99449f0296134808614d056c364ad25fad7d1
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-11-08 17:52:53 -0500

    Testing Herolu connection

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 })
 
 const PORT = process.env.PORT || 3000
