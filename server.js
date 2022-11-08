0de69f16b077df534a652342a12dbc6bf3ada4df
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-11-08 17:47:23 -0500

    Setup express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+	console.log(`Server running on port http://localhost:${PORT}`)
+})
